27d959866ceb5f73972372289a488eae062ed625
Rodri <berto01@gmail.com>
2019-09-22 14:31:03 -0300

    Test duplicado OK

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ¡¡¡¡Editar <code>src/App.js</code>  grabar y recargar!!!.
         </p>
         <a
           className="App-link"
